72011d278a7840d7862b847940a1bd4bcf73cbc4
Omar Osama <66084757+OmarKing015@users.noreply.github.com>
2023-12-08 10:32:26 +0000

    x

--- maththeif/src/App.jsx
+++ maththeif/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
     <h1>Click on session</h1>
-    <PureLectures style={{display:"felx", paddingRight: "400px"}}/>
+    <PureLectures style={{display:"felx", paddingRight: "500px"}}/>
 <Lectures/>
     </>
   )
